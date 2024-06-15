b70d43023c2d2f6263e602f57a6b9153bc0fd13a
Volodymyr <zmiyov88@gmail.com>
2024-06-15 12:44:26 +0300

    Initial Commit

---
+++ CursorGlowAnimationMacOS/CursorGlowAnimationMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorGlowAnimationMacOSApp.swift
+//  CursorGlowAnimationMacOS
+//
+//  Created by Volodymyr Pysarenko on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CursorGlowAnimationMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
